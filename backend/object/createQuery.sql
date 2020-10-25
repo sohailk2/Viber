a2e21d04fb129263fe2dcb07ecfc8940e5464370
Charlie Jung <32102793+charliecjung@users.noreply.github.com>
2020-10-25 12:54:33 -0500

    added initial sql commands from the json response

---
+++ backend/object/createQuery.sql
@@ -0,0 +1,55 @@
+
+CREATE TABLE USER (
+  Country VARCHAR(255),
+  Display_Name VARCHAR(255),
+  Email VARCHAR(255),
+  Id: INT
+)
+
+
+-----------------------------------------------------------
+Outdated below the line:
+Taken from a Spotify api (obsolete)? From Medium
+
+
+
+
+
+CREATE TABLE PERSON (
+  name VARCHAR(255),
+  Spotify_URI VARCHAR(255),
+  Spotify_ID VARCHAR(255),
+  Spotify_category_id VARCHAR(255),
+  Spotify_user_ID VARCHAR(255),
+  Spotify_URI VARCHAR(255)
+)
+
+CREATE TABLE ALBUMS(
+  AlbumName VARCHAR(255),
+  Album_Type INT,
+  Artist_Info VARCHAR(255),
+  Track_Info VARCHAR(255),
+  Images VARCHAR(255),
+  Release_Date VARCHAR(25),
+  Total_Tracks INT,
+  Popularity INT,
+  Preview_URL VARCHAR(255),
+  Spotify_URI VARHCAR(255),
+  Spotify_ID INT
+)
+
+CREATE TABLE Artists (
+  Artist_Name VARCHAR(255),
+  Genre VARCHAR(255),
+  Popularity_Score VARCHAR(255),
+  Followers INT,
+  Images VARCHAR(255),
+  Spotify_URI VARCHAR(255),
+  Spotify_ID INT
+)
+
+
+
+
+
+
