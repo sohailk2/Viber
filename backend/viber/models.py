f703ae432f15976de2dd9c072ced64357e38f873
Charlie Jung <32102793+charliecjung@users.noreply.github.com>
2020-10-31 14:58:09 -0500

    first commit

--- backend/viber/models.py
+++ backend/viber/models.py
@@ -2,3 +2,36 @@ from django.db import models
 
 # Create your models here.
 # the sql stuff?
+
+class Person(models.Model):
+    display_name = models.TextField(primary_key=True, blank=True, null=False)
+    #spotify?
+    external_urls_spotify = models.TextField(blank=True, null=True)
+    followers_count = models.IntegerField(blank=True, null=True)
+    followers_href = models.TextField(blank=True, null=True)
+    href = models.TextField(blank=True, null=True)
+    id = models.IntegerField(blank=True, null=True)
+    images = models.TextField(blank=True, null=True)
+    type = models.TextField(blank=True, null=True)
+    uri = models.TextField(blank=True, null=True)
+
+class PERSONS_TABLE:
+    managed = False
+    db_table = 'persons'
+def __str__(self):
+    return self.title
+# {
+#   "display_name": "charlie4",
+#   "external_urls": {
+#     "spotify": "https://open.spotify.com/user/charlie4"
+#   },
+#   "followers": {
+#     "href": null,
+#     "total": 20
+#   },
+#   "href": "https://api.spotify.com/v1/users/charlie4",
+#   "id": "charlie4",
+#   "images": [],
+#   "type": "user",
+#   "uri": "spotify:user:charlie4"
+# }
--- backend/viber/views.py
+++ backend/viber/views.py
@@ -8,10 +8,13 @@ def index(request):
     return HttpResponse("Hello, world. You're at the viber index.")
 
 def search(request):
+    #charlie4examplequery
+    person_example = Person.objects.filter(display_name="charlie4")
+    print("Person: ", person_example)
     returnVal = {"data": [
-                {"name": "song" +  str(random.randint(1,20)), "artist": "artist1"}, 
-                {"name": "song2" +  str(random.randint(1,20)), "artist": "artist2"}, 
-                {"name": "song3" +  str(random.randint(1,20)), "artist": "artist3"}, 
+                {"name": "song" +  str(random.randint(1,20)), "artist": "artist1"},
+                {"name": "song2" +  str(random.randint(1,20)), "artist": "artist2"},
+                {"name": "song3" +  str(random.randint(1,20)), "artist": "artist3"},
                 {"name": "song4" +  str(random.randint(1,20)), "artist": "artist4"}]}
 
     return JsonResponse(returnVal)
